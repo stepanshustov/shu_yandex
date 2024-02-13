3b33526b2d95459ec6d6e1e03e06e8d0273eec7c
YA#1 <step.shustov@mail.ru>
2024-02-13 19:25:05 +0400

    commit 6

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ from flask import *
 app = Flask(__name__)
 
 
+def check(st: str):
+    st = st.lower()
+    if "инженер" in st:
+        return 1
+    if "техн" in st:
+        return 1
+    if "програм" in st:
+        return 1
+    return 2
+
+
 @app.route('/')
 @app.route('/index/<tl>')
 @app.route('/index/')
 def ind(tl="заголовок"):
-    return render_template('index.html', title=tl)
+    return render_template('base.html', title=tl)
+
+
+@app.route('/training/<prof>')
+def tr(prof):
+    return render_template('index.html', number=check(prof), title="mars")
 
 
 if __name__ == '__main__':
